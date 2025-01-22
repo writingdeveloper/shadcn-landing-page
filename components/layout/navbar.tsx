f7556f10f5f11144579d7e6bddc3da5302722eec
Si Hyeong Lee <sihyeongdev@gmail.com>
2025-01-21 23:52:51 -0800

    Update navbar.tsx

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -66,7 +66,7 @@ export const Navbar = () => {
         <header className="shadow-inner bg-opacity-15 w-[90%] md:w-[70%] lg:w-[75%] lg:max-w-screen-xl top-5 mx-auto sticky border border-secondary z-40 rounded-2xl flex justify-between items-center p-2 bg-card">
             <Link href="/" className="font-bold text-lg flex items-center">
                 <ChevronsDown className="bg-gradient-to-tr border-secondary from-primary via-primary/70 to-primary rounded-lg w-9 h-9 mr-2 border text-white" />
-                Shadcn
+                Itembox
             </Link>
             {/* <!-- Mobile --> */}
             <div className="flex items-center lg:hidden">
